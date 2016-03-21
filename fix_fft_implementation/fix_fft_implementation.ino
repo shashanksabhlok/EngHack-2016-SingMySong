c9f03a70c6e1a8ab1b96e615c804375e889310ab
Shashank Sabhlok <shashanksabhlok95@gmail.com>
2016-03-20 23:36:09 -0400

    Comments updated - JA Version

--- fix_fft_implementation/fix_fft_implementation.ino
+++ fix_fft_implementation/fix_fft_implementation.ino
@@ -1,82 +1,76 @@
 
-#include <fix_fft.h>
-//int mic = A0; //electret
+#include "fix_fft.h"
 
 char im[128];
-char data[128]; 
-char data_avgs[128];
+char re[128];
+int input;
 
-//mix max val to map fft
-int valMin = 0;
-int valMax = 30;  
-//bias to reduce on low and increase on high  
-int bias = 0;//+- bias to output
-
-void setup(){
-//  pinMode(mic, INPUT);
-Serial.begin(9600);
+void setup()
+{
+  Serial.begin(9600);
 }
 
-void loop(){
-  int static i = 0;
-  static long tt;
-  int val;
-  randomSeed(analogRead(0));
- // bias = random(70,150);
-
-  for (i=0; i < 128; i++){  
-    val = analogRead(0);
-    //data[i] = val / 4 - 128;
-  data[i] = val;//quick and dirty data.
+void loop()
+{
+  
+  //Get input from mic
+  for (int i=0; i < 128; i++){
+    input = analogRead(0);
+    re[i] = input;
     im[i] = 0;
-    i++;   
-  }//end for
+  }
 
-  //this could be done with the fix_fftr function without the im array.
-  fix_fft(data,im,7,0);
-  /*
-   Performs foward or inverse Fourier transform.
-   //fix_fft (char fr[], char fi[], int m, int inverse)
-   //fr is a real data set,
-   //fi is the imaginary data set,
-   // m is log2(n) where n is number of data points (log2 (128) = 7)
-   //0 is set for forward transform. 1 would be inverse transform. Apparently inverse does not work,
-   */
-//  // I am only interested in the absolute value of the transformation
-//  for (i=0; i< 64;i++){//real val is for the amplitude 
-//    data[i] = sqrt(data[i] * data[i] + im[i] * im[i]); 
-//  }//end for
+  //Perform FFT
+  fix_fft(re,im,7,0);
 
-  //do something with the data values 1..64 and ignore im
-  //data avg moved to individual func
 
-  //shin: styles for colour mapping to frequency
-  //triChn(bias);
-  //triChn(0);//no bias
-  //style2
-  //grpChnLowPass();
-  //style3
-//  treshChn();
+  //Function to get frequency for max amplitude
+  int i,j,largest;
+  char str[129];
 
-  print_fft((char *)"Testing");
+  str[128] = 0;  
+  largest = 0;
+  // Find the largest entry which will determine how many lines
+  // are needed to print the whole histogram
+  for (i=0; i< 128;i++){
+    re[i] = sqrt(re[i] * re[i] + im[i] * im[i]);
+    if(re[i] > largest){
+      largest = re[i];
+      j = i;
+    }
+  }
 
-}//end loop
+  //If frequency is above our range, throw it away
+  if(j < 64){
+    int output = j*40*1.7443877551020408163265306122449;
+  
+  //Generate tone at frequency
+    tone(8,output,500);
 
+//  //Print frequency and generate graph
+//  Serial.println(j*40*1.7443877551020408163265306122449);
+//  print_fft((char *)"Output");
+
+    }
+	
+	//Delay to reduce feedback from microphone
+       delay(50);
+}
 
 // Generate the magnitude of each bin and print it on the serial monitor
 void print_fft(char *title)
 {
   int i,j,largest;
-  char str[65];
+  char str[129];
   char linfo[6];
 
-  str[64] = 0;  
+  str[128] = 0;  
   largest = 0;
   // Find the largest entry which will determine how many lines
   // are needed to print the whole histogram
-  for (i=0; i< 64;i++){
-    data[i] = sqrt(data[i] * data[i] + im[i] * im[i]);
-    if(data[i] > largest)largest = data[i];
+  for (i=0; i< 128;i++){
+    re[i] = sqrt(re[i] * re[i] + im[i] * im[i]);
+    if(re[i] > largest)largest = re[i];
   }
   // print a blank line just in case there's
   // garbage when the Serial monitor starts up
@@ -87,10 +81,10 @@ void print_fft(char *title)
   // print the histogram starting with the highest amplitude
   // and working our way back down to zero.
   for(j=largest;j >= 0;j--) {
-    for(i=0;i<64;i++) {
+    for(i=0;i<128;i++) {
       // If the magnitude of this bin is at least as large as
       // the current magnitude we print an asterisk
-      if(data[i] >= j)str[i] = '*';
+      if(re[i] >= j)str[i] = '*';
       // otherwise print a space
       else str[i] = ' ';
     }
@@ -100,12 +94,12 @@ void print_fft(char *title)
   }
   // print the bin numbers along the bottom
   Serial.print("    ");
-  for(i=0;i<64;i++) {
+  for(i=0;i<128;i++) {
     Serial.print(i%10);
   }
   Serial.println("");
   Serial.print("    ");
-  for(i=0;i<64;i++) {
+  for(i=0;i<128;i++) {
     if(i < 10)Serial.print(" ");
     else Serial.print((i/10)%10);
   }
